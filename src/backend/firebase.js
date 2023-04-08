75f682b2c98898655cb299f4469c540e961feef7
Andre Espinoza <ae95caba@gmail.com>
2023-04-08 04:28:40 -0300

    add firebase app check with recatpcha

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -27,124 +28,11 @@ export const storage = getStorage();
 
 export const db = getFirestore(app);
 
-///////////////////////////////////////////
-
-/* const signupform = document.querySelector("#signup-form");
-
-signupform.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const signupEmail = document.getElementById("signup-email").value;
-  const signupPassword = document.getElementById("signup-password").value;
-
-  createUserWithEmailAndPassword(auth, signupEmail, signupPassword).then(
-    (userCredential) => {
-      alert("sign up");
-    }
-  );
-}); */
-
-////////////////////////////////////////////////////////////////////////
-
-/* const loginform = document.querySelector("#login-form");
-
-loginform.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const loginEmail = document.getElementById("login-email").value;
-  const loginPassword = document.getElementById("login-password").value;
-
-  signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-    .then((userCredential) => {
-      alert("te loggeaste perro");
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
-}); */
-
-////////////////////////////////////////////////////////////////////////
-
-/* const logout = document.getElementById("logout");
-
-logout.addEventListener("click", (e) => {
-  e.preventDefault();
-  signOut(auth)
-    .then(() => {
-      console.log("saliste perri");
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6LeAfWwlAAAAAMMbgpAKdW9h1NuACZHwyy7V5ywC"),
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true,
 });
- */
-////////////////////////////////////////////
-//subscribing to auth state changes
-
-/* auth.onAuthStateChanged((user) => {
-  alert("auth state lisener");
-  if (user) {
-    console.log("hay un usuario logeado");
-    console.log(user);
-  } else {
-    console.log("no hay un usuario activo");
-  }
-}); */
-
-////////////////////////////////////////////
-
-/* const docs = await getDocs(collection(fs, "helados"));
-console.log(docs); */
-
-////////////////////////////////////
-
-// google login
-
-/* const provider = new GoogleAuthProvider();
-
-const googleButton = document.getElementById("googleLogin");
-googleButton.addEventListener("click", (e) => {
-  alert("click google");
-  signInWithRedirect(auth, provider)
-    .then((result) => {
-      console.log("te logeaste con google");
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    })
-    .catch((error) => {
-      console.log(error.message);
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-}); */
-
-/////////////////////////////////////////////
-
-/* function userinfo() {
-  const user = auth.currentUser;
-  console.log(user);
-  if (user !== null) {
-    // The user object has basic properties
-    // such as display name, email, etc.
-    const displayName = user.displayName;
-    const email = user.email;
-    const photoURL = user.photoURL;
-    const emailVerified = user.emailVerified;
-    const uid = user.uid;
-    alert(email);
-  }
-}
-
-setTimeout(() => {
-  alert("timeout");
-  userinfo();
-}, 3000); */
