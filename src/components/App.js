b97161c4ab4e4781b0ea6a95d7aaa8dd16749598
Andre Espinoza <ae95caba@gmail.com>
2023-04-13 21:26:25 -0300

    remove addProductsToFirestore from app.js

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ export const App = () => {
   });
   const [flavours, setFlavours] = useState(null);
 
-  addProductsToFirestore(productsObj);
+  /*  addProductsToFirestore(productsObj); */
 
   useEffect(() => {
     function convertStringToArray(string) {
