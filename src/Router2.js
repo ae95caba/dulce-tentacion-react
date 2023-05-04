2131605c13a9729e75755752af50f7fd6bd91600
Andre Espinoza <ae95caba@gmail.com>
2023-05-04 12:46:32 -0300

    fix logo again

--- src/Router2.js
+++ src/Router2.js
@@ -6,7 +6,9 @@ import { App } from "./components/App";
 export function Router2() {
   return (
     <BrowserRouter>
+      <div id="left-bar"></div>
       <App />
+      <div id="right-bar"></div>
     </BrowserRouter>
   );
 }
