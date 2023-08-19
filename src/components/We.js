b9765fe31dbfaa843411671cd010b5c510b1e600
ae95caba <ae95caba@gmail.com>
2023-08-19 09:08:07 -0300

    minor css changes to make it look better

--- src/components/We.js
+++ src/components/We.js
@@ -9,7 +9,7 @@ export default function We() {
         Helado artesanal es sinónimo de <strong>calidad</strong>. Los helados
         artesanales se elaboran en un obrador, básicamente con procedimientos
         manuales. En su elaboración se emplea únicamente
-        <strong>materia prima fresca y local</strong>, y al contrario de los
+        <strong> materia prima fresca y local</strong>, y al contrario de los
         helados industriales, no se utilizan saborizantes, colorantes, ni
         conservantes.
         <strong> Tienen mucho menos aire y mucha menos grasa</strong> (sólo 7%)
@@ -25,15 +25,15 @@ export default function We() {
         usan colorantes y saborizantes para realzar su aspecto y sabor. El
         heladero industrial busca fabricar un producto con el menor coste
         posible, el objetivo es maximizar la rentabilidad. Por eso se
-        <strong>utilizan ingredientes de menor calidad</strong> y procedimientos
-        como los siguientes: se sustituye la leche por sólido lácteo, la nata
-        por grasa de coco, y sobre todo{" "}
+        <strong> utilizan ingredientes de menor calidad</strong> y
+        procedimientos como los siguientes: se sustituye la leche por sólido
+        lácteo, la nata por grasa de coco, y sobre todo{" "}
         <strong>llevan una gran cantidad de aire incorporado</strong> (hasta
         100%). De esta forma sacan un mayor rendimiento al producto, pero
         rebajando la calidad comparado con el helado artesano. Gracias a su
         producción masiva, es uno de los más económicos. Lleva también mucha
         grasa (hasta 30%) de origen vegetal. Suelen ser menos cremosos que un
-        helado artesanal. Por lo tanto,
+        helado artesanal. Por lo tanto,{" "}
         <strong>
           al tratarse de helados ultraprocesados, nutricionalmente son poco
           recomendables
